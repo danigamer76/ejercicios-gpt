de8a64d2e65690abaa30cdcf3d966dbcc3a127ec
Daniel Guardia <83751029+danigamer76@users.noreply.github.com>
2025-02-12 18:21:38 +0100

    Dia 5

---
+++ Proyecto_Simulador_de_Tienda_Online/tienda.js
@@ -0,0 +1,11 @@
+function iniciarSesion(){
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value; 
+    console.log("Nombre ingresado:" + nombre);
+    console.log("Email ingresado:" + email);
+
+}
+
+function registrarUsuario(){
+    alert("REGISTRAR USUARIO");
+}
